5711fd8ce837c91d6c07fe6c2f18b87179a97116
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-12-05 13:54:42 +0300

    new

---
+++ DBConnect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace exampk42
+{
+    internal static class DBConnect
+    {
+        static MySqlConnection conn;
+        public static MySqlConnection Connection()
+        {
+            var connection = new MySqlConnection("Server=localhost;User ID=student;Password=student;Database=exampk42");
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace exampk42
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = DBConnect.Connection();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT id FROM users WHERE login = @login";
+            cmd.Parameters.AddWithValue("login", textBox1.Text.Trim());
+            cmd.ExecuteNonQuery();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            Debug.WriteLine(reader.HasRows);
+        }
+    }
+}
